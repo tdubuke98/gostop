16b9aae4253bcd0ba21e513ce4602238fd56ac2e
Ubuntu <ubuntu@ip-172-31-30-44.ec2.internal>
2025-09-16 17:18:48 +0000

    fixed docker things

--- backend/run.sh
+++ backend/run.sh
@@ -5,7 +5,11 @@ docker build -t gostop_backend .
 
 # Run the docker container
 docker run -d \
-	--env DATABASE_PATH="/data/.data.DEFAULT.db" \
+	-e DATABASE_PATH="/data/.data.DEFAULT.db" \
+        -e DD_SERVICE=gostop_backend \
+	-e DD_ENV=tdub_aws \
+	-e DD_LOGS_INJECTION=true \
+       	-e DD_PROFILING_ENABLED=true \
 	--name gostop_backend_container \
 	--mount source=gostop_db,target=/data \
 	-p 8000:8000 \
